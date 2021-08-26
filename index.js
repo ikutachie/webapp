9eea97138bae4df866d546088a79263eeefcb937
生田　知恵 <chie-ikuta@example.com>
2021-08-26 17:21:12 +0900

    test

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
 const index = fs.readFileSync('./index.ejs','utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
+
 var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('server start');
 
-function getFromClient(reqest, response){
-  var content = ejs.render(index);
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(content);
-  response.end();
+function getFromClient(request, response){
+  var url_parts = url.parse(request.url);
+  switch(url_parts.pathname){
+   case'/':
+     var content = ejs.render(index,{
+      title: "Indexページ",
+      content: "これはテンプレートを使ったサンプルページです"
+     });
+     response.writeHead(200, {'Content-Type': 'text/html'});
+     response.write(content);
+     response.end();
+     break;
+
+    case'/style.css':
+      response.writeHead(200, {'Content-Type': 'text/css'});
+      response.write(style_css);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('no page...');
+      break;
+}
 }
